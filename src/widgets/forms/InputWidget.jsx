41df6bc4ded179221b1e6a840b360582222ba2e3
Marta Martinez Real <mmartinezr@oasys-sw.com>
2022-03-10 16:51:42 +0100

    update InputWidget

--- src/widgets/forms/InputWidget.jsx
+++ src/widgets/forms/InputWidget.jsx
@@ -1,8 +1,21 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
+import InputLabel from "@mui/material/InputLabel";
 import { TextField } from "@mui/material";
 import { formContext } from "../../context/ContextProvider";
 
-const InputWidget = ({ formId, id, label }) => {
+const InputWidget = ({
+  formId,
+  id,
+  label,
+  required,
+  multiline,
+  type,
+  maxLength,
+  disabled,
+  placeholder,
+  min,
+  max,
+}) => {
   const { formWidget, setformWidget } = useContext(formContext);
   const [localVal, setLocalVal] = useState(formWidget?.[formId]?.[id]);
   //blur event added, to fix performance issues
@@ -12,15 +25,41 @@ const InputWidget = ({ formId, id, label }) => {
     setformWidget({ ...formWidget, [formId]: formToChange });
   };
 
+  const handleChange = (e) => {
+    if (min != null && max != null && type === "number") {
+      var value = parseInt(e.target.value, 10);
+
+      if (value > max) value = max;
+      if (value < min) value = min;
+
+      setLocalVal(value);
+    } else {
+      setLocalVal(e.target.value);
+    }
+  };
+
   return (
-    <TextField
-      required
-      id={`${formId}-textField-${id}-label`}
-      label={label}
-      value={localVal}
-      onChange={(e) => setLocalVal(e.target.value)}
-      onBlur={handleBlur}
-    />
+    <>
+      <InputLabel required={required}>{label}</InputLabel>
+      <TextField
+        required={required}
+        id={`${formId}-textField-${id}-label`}
+        value={localVal}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        multiline={multiline}
+        type={type}
+        fullWidth
+        inputProps={{
+          maxLength: maxLength,
+          min: min,
+          max: max,
+          autoComplete: "new-password",
+        }}
+        disabled={disabled}
+        placeholder={placeholder}
+      />
+    </>
   );
 };
 
