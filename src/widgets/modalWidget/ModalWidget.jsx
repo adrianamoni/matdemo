943cb8248fc123bd8b5259505296bff9f4188bb5
Marta Martinez Real <mmartinezr@oasys-sw.com>
2022-03-10 16:51:51 +0100

    update ModalWidget

--- src/widgets/modalWidget/ModalWidget.jsx
+++ src/widgets/modalWidget/ModalWidget.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Modal, Paper } from "@mui/material";
 
-const ModalWidget = ({ open, close, content }) => {
+const ModalWidget = ({ open, close, content, customWidth }) => {
   const style = {
     position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: customWidth,
     bgcolor: "background.paper",
     boxShadow: 24,
     p: 4,
