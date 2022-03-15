94af42f829267320f9550f320c6d100c4e294286
Marta Martinez Real <mmartinezr@oasys-sw.com>
2022-03-15 13:54:49 +0100

    CustomSnackBar

---
+++ src/components/alerts/CustomSnackBar.jsx
@@ -0,0 +1,42 @@
+﻿import React from "react";
+import { Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const CustomSnackBar = ({ snackBarAlert, setSnackBarAlert }) => {
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackBarAlert({
+      open: false,
+      message: "",
+      severity: "",
+    });
+  };
+
+  return (
+    <>
+      <Snackbar
+        open={snackBarAlert.open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        sx={{ width: "100%" }}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={snackBarAlert.severity}
+          sx={{ width: "100%" }}
+        >
+          {snackBarAlert.message}
+        </Alert>
+      </Snackbar>
+    </>
+  );
+};
+
+export default CustomSnackBar;
