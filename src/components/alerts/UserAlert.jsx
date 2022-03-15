c3e7982f6562a94764a6686459224080b8861ef5
Marta Martinez Real <mmartinezr@oasys-sw.com>
2022-03-15 17:47:52 +0100

    UserAlert

---
+++ src/components/alerts/UserAlert.jsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import { Alert } from "@mui/material";
+
+const UserAlert = ({ severity, message }) => {
+  return (
+    <Alert variant="filled" severity={severity}>
+      {message}
+    </Alert>
+  );
+};
+
+export default UserAlert;
